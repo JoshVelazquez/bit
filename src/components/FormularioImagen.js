a1380d0fcd590a51dc4218ab55aa1cd561885c59
Annie <joshua.velazque@alumnos.udg.mx>
2020-12-04 15:09:19 -0600

    Correccion del nombre del archivo en el input

--- src/components/FormularioImagen.js
+++ src/components/FormularioImagen.js
@@ -1,33 +1,55 @@
-import React, { Component } from 'react';
-import '../assets/css/FormularioImagen.css';
+import React, { Component } from "react";
+import "../assets/css/FormularioImagen.css";
 
 class FormularioImagen extends Component {
+  constructor(props) {
+    super(props);
+    this.fileInput = React.createRef();
+    this.state = {
+      textoLabel: "Seleccione un archivo",
+    };
+  }
 
-    constructor(props) {
-        super(props);
-        this.fileInput = React.createRef();
-      }
+  handleChange = (event) =>{
+    this.setState({ textoLabel: event.target.files[0].name });
+  }
 
-    render() {
-        return (
-            <div className="col-md-6">
-                <div className="card border-rounded border-dark shadow">
-                    <div className="card-header encabezado-formulario text-center">
-                        <h5>Ingrese una imagen valida</h5>
-                </div>
-                    <div className="card-body">
-                        <div className="input-group">
-                            <form className="custom-file" action="" method="">
-                                <input type="file" className="custom-file-input" id="inputGroupFile01" aria-describedby="inputGroupFileAddon01"/>
-                                <label className="custom-file-label" for="inputGroupFile01">Enviar</label>
-                                <button className="btn btn-purple btn mt-2" href="#0">Enviar</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="col-md-6">
+        <div className="card border-rounded border-dark shadow">
+          <div className="card-header encabezado-formulario text-center">
+            <h5>Ingrese una imagen valida</h5>
+          </div>
+          <div className="card-body">
+            <div className="input-group">
+              <div className="custom-file">
+                <input
+                  type="file"
+                  className="custom-file-input"
+                  id="inputGroupFile01"
+                  onChange={this.handleChange}
+                  aria-describedby="inputGroupFileAddon01"
+                />
+                <label className="custom-file-label" htmlFor="inputGroupFile01">
+                  {this.state.textoLabel}
+                </label>
+              </div>
+              <div className="input-group-append">
+                <button
+                  className="btn btn-purple btn"
+                  href="#0"
+                  id="inputGroupFileAddon02"
+                >
+                  Enviar
+                </button>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FormularioImagen;
